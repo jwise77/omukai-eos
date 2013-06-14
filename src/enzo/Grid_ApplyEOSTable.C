414e68ce20faa796802b9d2aed0b881f339af567
John Wise <jwise@physics.gatech.edu>
2013-06-14 18:05:45 -0400

    Temporary working version of EOS table lookup.  Causes MULTI_COOL max. iteration warnings (for now!).

--- src/enzo/Grid_ApplyEOSTable.C
+++ src/enzo/Grid_ApplyEOSTable.C
@@ -71,12 +71,12 @@ int grid::ApplyEOSTable(const float* TotalMetals)
      with the EOS temperature */
 
   int i, dim, size;
-  for (dim = 0; size = 1; dim < GridRank; dim++)
+  for (dim = 0, size = 1; dim < GridRank; dim++)
     size *= GridDimension[dim];
 
   int dindex, zindex, dindex1, zindex1;
   float dfactor, zfactor, drho, dz, drho_inv, dz_inv, rho_thres, z0, rho0, Z_H, 
-    log_rho, dom, new_temperature, mu;
+    log_rho, dom, new_temperature, mu, nH2, nother, x, gamma2, number_density, KE;
   rho_thres = MinimumDensityForEOSTable * mh / DensityUnits;
   drho = (CoolData.EOS_DensityRange[1] - CoolData.EOS_DensityRange[0]) / 
     (CoolData.EOS_NumberOfDensityBins - 1);
@@ -91,8 +91,22 @@ int grid::ApplyEOSTable(const float* TotalMetals)
     // If density is greater than threshold value, modify thermal energy.
     if (BaryonField[DensNum][i] > rho_thres) {
 
+      // Calculate mean molecular weight
+      if (MultiSpecies == 0) {
+	mu = Mu;
+      } else {
+	if (MultiSpecies > 0)
+	  number_density = BaryonField[DeNum][i] + BaryonField[HINum][i] + BaryonField[HIINum][i] + 
+	    0.25 * (BaryonField[HeINum][i] + BaryonField[HeIINum][i] + BaryonField[HeIIINum][i]);
+	if (MultiSpecies > 1)
+	  number_density += BaryonField[HMNum][i] + 0.5 * (BaryonField[H2INum][i] + BaryonField[H2IINum][i]);
+	if (MultiSpecies > 2)
+	  number_density += BaryonField[DINum][i] + BaryonField[DIINum][i] + 0.5 * BaryonField[HDINum][i];
+	mu = BaryonField[DensNum][i] / number_density;
+      } // ENDELSE
+
       // Calculate interpolation values for density
-      log_rho = log10(BaryonField[DensNum][i] * dom);
+      log_rho = log10(BaryonField[DensNum][i] * dom / mu);
       log_rho = min(max(log_rho, CoolData.EOS_DensityRange[0]), CoolData.EOS_DensityRange[1]);
       dindex = int((log_rho - CoolData.EOS_DensityRange[0]) * drho_inv);
       rho0 = CoolData.EOS_DensityRange[0] + drho * dindex;
@@ -117,20 +131,38 @@ int grid::ApplyEOSTable(const float* TotalMetals)
 	CoolData.EOS_Table[zindex1][dindex ] * (zfactor)     * (1 - dfactor) +
 	CoolData.EOS_Table[zindex ][dindex1] * (1 - zfactor) * (dfactor) +
 	CoolData.EOS_Table[zindex1][dindex1] * (zfactor)     * (dfactor);
-
-      if (MultiSpecies == 0) {
-	mu = Mu;
-      } else {
-	if (MultiSpecies > 0)
-	  mu = BaryonField[DeNum][i] + BaryonField[HINum][i] + BaryonField[HIINum] + 
-	    4.0 * (BaryonField[HeINum][i] + BaryonField[HeIINum][i] + BaryonField[HeIIINum][i]);
-	if (MultiSpecies > 1)
-	  mu += BaryonField[HMNum][i] + 2.0 * (BaryonField[H2INum][i] + BaryonField[H2IINum][i]);
-	if (MultiSpecies > 2)
-	  mu += BaryonField[DINum][i] + BaryonField[DIINum][i] + 2.0 * BaryonField[HDINum][i];
-	mu /= BaryonField[DensNum][i];
-      } // ENDELSE
       
+      /* Correct temperature for non-ideal gas effects for high density H2 */
+
+      if (MultiSpecies > 1) {
+	nH2 = 0.5 * (BaryonField[H2INum][i] + BaryonField[H2IINum][i]);
+	nother = 0.25 * (BaryonField[HeINum][i] + BaryonField[HeIINum][i] + BaryonField[HeIIINum][i]) +
+	  BaryonField[HINum][i] + BaryonField[HIINum][i] + BaryonField[DeNum][i];
+	if (nH2 / nother > 1e-3) {
+	  x = 6100.0 / new_temperature;
+	  if (x > 10.0)
+	    gamma2 = 2.5;
+	  else
+	    gamma2 = 0.5 * (5.0 + 2.0 * x*x * exp(x) / POW((exp(x) - 1.0), 2));
+	} else {
+	  gamma2 = 2.5;
+	} // ENDELSE
+	gamma2 = 1.0 + (nH2 + nother) / (nH2*gamma2 + nother/(Gamma - 1.0));
+      } else {
+	// Multispecies <= 1
+	gamma2 = Gamma;
+      }
+
+      if (DualEnergyFormalism) {
+	KE = BaryonField[TENum][i] - BaryonField[GENum][i];
+	BaryonField[GENum][i] = new_temperature / (TemperatureUnits * mu * (gamma2 - 1.0));
+	BaryonField[TENum][i] = BaryonField[GENum][i] + KE;
+      } else {
+	KE = 0.5 * (BaryonField[Vel1Num][i] * BaryonField[Vel1Num][i] +
+		    BaryonField[Vel2Num][i] * BaryonField[Vel2Num][i] +
+		    BaryonField[Vel3Num][i] * BaryonField[Vel3Num][i]);
+	BaryonField[TENum][i] = KE + new_temperature / (TemperatureUnits * mu * (gamma2 - 1.0));
+      }
       
     } // ENDIF overdense
 
